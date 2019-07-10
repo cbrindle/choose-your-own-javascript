75141fb611f044a2b9cd27882504fc89ce79cc2a
Christopher Brindle <christopherbrindle@Christophers-MacBook-Pro.local>
2019-07-10 16:41:06 -0400

    added if statements to index.html

--- main.js
+++ main.js
@@ -5,9 +5,15 @@ function handleDecision(event) {
   event.preventDefault();
 
   const inputBox = document.querySelector('.user-input');
-  const userInput = inputBox.value;
+  let userInput = inputBox.value;
+  let userInput = userInput.toLowerCase();
 
-  if (userInput === 'east') {
-    window.location = './east.html';
+  if (userInput.includes(`car`)) || (userInput.includes(`investigate`)) {
+    window.location = './investigate.html';
+  } else if (userInput.includes(`inside`)) {
+    window.location = './go-inside.html';
+  } else {
+    alert(`I dont' see how ` + userInput + ` will help. Try again.`);
+    return false;
   }
 }
