a6671345f977303a34f3be3c046005f81fb80a24
Christopher Brindle <christopherbrindle@Christophers-MacBook-Pro.local>
2019-07-11 08:59:00 -0400

    added content to liquor-choice html page

---
+++ js/liquor-choice.js
@@ -0,0 +1,23 @@
+const submitButton = document.querySelector('.submit');
+submitButton.onclick = handleDecision;
+
+function handleDecision(event) {
+  event.preventDefault();
+
+  const inputBox = document.querySelector('.user-input');
+  let answer = inputBox.value;
+  answer = Number(answer);
+
+  console.log(answer);
+
+  // Queries for index.html
+  if (answer === 1) {
+      window.location = './small-glass.html';
+  } else if (answer === 2) {
+      window.location = "./medium-glass.html";
+  } else if (answer === 3) {
+      window.location = "./large-glass.html";
+  } else {
+      alert(`I don't see how "` + answer + `" will help. Try again.`);
+  }
+}
