16cb1a630b602fe45eed55410f600c3e4c147d8f
Christopher Brindle <christopherbrindle@christophersmbp.lan>
2019-07-11 09:51:23 -0400

    added content to large-glass html page

---
+++ js/large-glass.js
@@ -0,0 +1,19 @@
+const submitButton = document.querySelector('.submit');
+submitButton.onclick = handleDecision;
+
+function handleDecision(event) {
+  event.preventDefault();
+
+  const inputBox = document.querySelector('.user-input');
+  let answer = inputBox.value;
+  answer = answer.toLowerCase();
+
+  console.log(answer);
+
+  // Queries for index.html
+//   if (answer === `back`) {
+//       window.location = './liquor-choice.html';
+//   } else {
+//     alert(`I don't see how "` + answer + `" will help. Try again.`);
+//   }
+// }
