84345e60f1f84f62b3a9097b37e0ae15c2cd2194
Christopher Brindle <christopherbrindle@Christophers-MacBook-Pro.local>
2019-07-11 08:37:28 -0400

    added content to go-inside html page

---
+++ js/go-inside.js
@@ -0,0 +1,21 @@
+const submitButton = document.querySelector('.submit');
+submitButton.onclick = handleDecision;
+
+function handleDecision(event) {
+  event.preventDefault();
+
+  const inputBox = document.querySelector('.user-input');
+  let answer = inputBox.value;
+  answer = answer.toLowerCase();
+
+  console.log(answer);
+
+  // Queries for index.html
+  if (answer.includes(`hide`) || (answer.includes(`bushes`)) || (answer.includes(`scare`))) {
+    window.location = './hide.html';
+  } else if (answer.includes(`greet`) || answer.includes(`run`)) {
+    window.location = './greet.html';
+  } else {
+    alert(`I don't see how "` + answer + `" will help. Try again.`);
+  }
+}
