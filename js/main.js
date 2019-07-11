bcc76f2367c028dfb78880e7de7520dfa3b73fdd
Christopher Brindle <christopherbrindle@ChristophersMBP.fios-router.home>
2019-07-10 20:30:26 -0400

    pushing to correct path issues

---
+++ js/main.js
@@ -0,0 +1,21 @@
+const submitButton = document.querySelector('.submit');
+submitButton.onclick = handleDecision;
+
+function handleDecision(event) {
+  event.preventDefault();
+
+  const inputBox = document.querySelector('.user-input');
+  let answer = inputBox.value;
+  answer = answer.toLowerCase();
+
+  console.log(answer);
+
+  // Queries for index.html
+  if (answer.includes(`car`) || (answer.includes(`investigate`))) {
+    window.location = './investigate.html';
+  } else if (answer.includes(`inside`)) {
+    window.location = './go-inside.html';
+  } else {
+    alert(`I don't see how ` + answer + ` will help. Try again.`);
+  }
+}
